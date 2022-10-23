007b7a734565b7304a66c3bdfe02bebf4cdad67d
Dmitry Lavrinenko <keldwar@yahoo.com>
2022-10-23 17:27:40 +0500

    task2

--- src/main/java/org/example/tgbot/Main.java
+++ src/main/java/org/example/tgbot/Main.java
@@ -1,27 +1,39 @@
 package org.example.tgbot;
 
-import org.example.tgbot.dto.Request;
-import org.example.tgbot.dto.Response;
-import org.example.tgbot.handlers.Handler;
-import org.example.tgbot.handlers.SimpleHandler;
-import org.example.tgbot.readers.ConsoleReader;
-import org.example.tgbot.readers.Reader;
-import org.example.tgbot.writers.ConsoleWriter;
-import org.example.tgbot.writers.Writer;
+
+import org.example.tgbot.telegramBot.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
-        Reader reader = new ConsoleReader();
-        Writer writer = new ConsoleWriter();
-        Handler handler = new SimpleHandler();
+    public static String getTkFromProperty(String fileName) {
+        //работа с конфиг файлом
+        FileInputStream fis;
+        Properties property = new Properties();
 
-        while (true) {
-            Request request = reader.read();
-            if (request.data().isEmpty()) {
-                break;
-            }
-            Response response = handler.handleRequest(request);
-            writer.write(response);
+        try {
+            fis = new FileInputStream("src/main/resources/" + fileName);
+            property.load(fis);
+            fis.close();
+        } catch (IOException e) {
+            System.err.println("ОШИБКА: Файл свойств отсуствует!");
         }
+
+        return property.getProperty("token");
+    }
+
+    public static void startTgBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        TelegramBot bot = new TelegramBot();
+        botsApi.registerBot(bot);
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        startTgBot();
     }
 }
