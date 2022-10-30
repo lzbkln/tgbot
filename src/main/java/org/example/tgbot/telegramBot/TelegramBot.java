899acbf6eabf4462fef268ea8a531a8b2282f7f4
Dmitry Lavrinenko <keldwar@yahoo.com>
2022-10-30 16:18:35 +0500

    Some needed changes

--- src/main/java/org/example/tgbot/telegramBot/TelegramBot.java
+++ src/main/java/org/example/tgbot/telegramBot/TelegramBot.java
@@ -3,27 +3,24 @@ package org.example.tgbot.telegramBot;
 import org.example.tgbot.Main;
 import org.example.tgbot.dto.Request;
 import org.example.tgbot.dto.Response;
-import org.example.tgbot.handlers.TelegramHandler;
+import org.example.tgbot.handlers.SimpleHandler;
 import org.example.tgbot.readers.TelegramReader;
 import org.example.tgbot.writers.TelegramWriter;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
-    //маппер
-    //компонент
+
     TelegramReader telegramReader;
     TelegramWriter telegramWriter;
-    TelegramHandler telegramHandler;
+    SimpleHandler simpleHandler;
 
     public TelegramBot() {
         telegramReader = new TelegramReader();
         telegramWriter = new TelegramWriter();
-        telegramHandler = new TelegramHandler();
+        simpleHandler = new SimpleHandler();
     }
 
     public static final String botName = "RomanceClubGuides";
@@ -47,43 +44,12 @@ public class TelegramBot extends TelegramLongPollingBot {
         var id = user.getId();
         if (!update.getMessage().hasText()) {
             System.out.println("It hasn't text");
-            // copyMessage(id, msg.getMessageId());
             return;
         }
         request = telegramReader.read(update);
-        response = telegramHandler.handleRequest(request);
-        //System.out.println(update);
-
-        //System.out.println((user.getId()));
-
+        response = simpleHandler.handleRequest(request);
         SendMessage sendMes = telegramWriter.createMessage(response, id);
         sendMessage(sendMes);
-        //эхо ответ
-        //copyMessage(id, msg.getMessageId());
-    }
-
-    public void copyMessage(Long who, Integer msgId) {
-        CopyMessage cm = CopyMessage.builder()
-                .fromChatId(who.toString())
-                .chatId(who.toString())
-                .messageId(msgId)
-                .build();
-        try {
-            execute(cm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void sendText(Long who, String what) {
-        SendMessage sm = SendMessage.builder()
-                .chatId(who.toString())
-                .text(what).build();
-        try {
-            execute(sm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     //пока только с текстовыми сообщениями
--- src/main/java/org/example/tgbot/writers/TelegramWriter.java
+++ src/main/java/org/example/tgbot/writers/TelegramWriter.java
@@ -1,17 +1,43 @@
 package org.example.tgbot.writers;
 
 import org.example.tgbot.dto.Response;
-import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.bots.AbsSender;
 public class TelegramWriter {
-    //отдельный дата класс, хранит данные пользователя, как поле реквеста
     public SendMessage createMessage(Response response, Long who) {
-        SendMessage sm = SendMessage.builder()
+        return SendMessage.builder()
                 .chatId(who.toString())
                 .text(response.data()).build();
-        return sm;
     }
 }
-//тг сендер (фасад), разные сендеры для разных сообщений
-//можно реализовать writer ботом и собственно сделать норм метод write для бота
+/*
+1. Сделать Request, Response дата классами, но не через record,
+record тут не сильно удобен. После задачи 6 создать конструкт для MetaData.
+
+2. На данный момент TelegramWriter создает сообщение, а не пишет его, что плохо.
+Возможно, лучше сделать TelegramDataConverter, который объединит текущие функции
+TelegramReader и TelegramWriter, то есть read и createMessage. Надо обсудить.
+
+3. Паттерн компонент: Request и Response должны содержать в себе Component[].
+Component - это общий класс, от которого наследуются все нужные нам данные.
+Это позволит с любыми данными работать одинаково. К тому же
+может прийти сообщение с фоткой и текстом, а мы не хотим потерять данные. Надо обсудить.
+
+4. Другое решение задачи 2: реализуем интерфейс Writer нашим ботом, переопределяем метод write,
+так, чтобы именно он корректно отправлял сообщение на основе данных. Но тогда придется
+конструировать сообщение в этом методе, что опять же плохо. Надо что-то придумать.
+
+5. Еще одно решение задачи 2 и 4: используем паттерн фасад. TelegramWriter делаем фасадом
+и создаем разные Sender`ы для разных видов сообщений (MessageSender, PhotoSender, EmojiSender).
+Пока остановимся на MessageSender. При этом где-то должны создаваться сами сообщения (пока наверно
+в соответствующем Sender`е, либо вынести это в какой-нибудь TelegramCreator),
+а затем отправляться через соответствующий Sender TelegramWriter`а. Надо обсудить.
+
+6. Создать класс метаданных и сделать его полем Request, Response.
+Либо к Request и Response добавить необходимые поля (id, msgID, username, ...).
+маппер: отображение одной сущности в другую, TelegramReader, TelegramWriter сделать
+
+7. На данном этапе должен работать обработчик SimpleHandler, а не TelegramHandler,
+поскольку мы хотим лишь эхо ответ, бы обработки команд.
+*/
+
+
