eb104e183d43067834017713fa95b9700e69d7f7
lzbkl <lzbkln@mail.ru>
2022-11-28 12:42:13 +0500

    Изменение текста для команды /help, добавлен вывод историй для команды /stories

---
+++ src/main/java/org/dl/tgbot/parse/Story.java
@@ -0,0 +1,63 @@
+package org.dl.tgbot.parse;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Story implements Page {
+    protected static String name;
+
+    public HashMap<String, String> makeDictNames() throws FileNotFoundException {
+        String path = "C:/Users/lzbkl/IdeaProjects/tgbot/src/main/resources/stories";
+        File file = new File(path);
+        Scanner scannerF = new Scanner(file);
+        HashMap<String, String> linkNames = new HashMap<>();
+        while (scannerF.hasNextLine()) {
+            String line = scannerF.nextLine();
+            Pattern pattern = Pattern.compile ("\"(.+?)\"");
+            Matcher matcher = pattern.matcher (line);
+            int i = 0;
+            String[] pair = new String[2];
+            while (matcher.find()) {
+                pair[i] = matcher.group(1);
+                i++;
+            }
+            linkNames.put(pair[0], pair[1]);
+        }
+        return linkNames;
+    }
+
+    public ArrayList<String> printTitles() throws FileNotFoundException {
+        return new ArrayList<>(makeDictNames().keySet());
+    }
+
+    public String getPage(String link)  {
+        StringBuilder page = new StringBuilder();
+        BufferedReader in = null;
+        try {
+            URL url = new URL(link);
+            in = new BufferedReader(new InputStreamReader(url.openStream(),"cp1251")); //позволяет создать входной поток для чтения файла ресурса, связанного с созданным объектом класса URL
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                page.append(inputLine);
+            }
+            in.close();
+        }
+        catch (IOException e){
+            System.out.println("IOException");
+        }
+        finally {
+            try{
+                if (in != null){
+                    in.close();
+                }
+            }
+            catch (IOException e){
+                System.out.println("IOException");
+            }
+
+        }
+        return page.toString();
+    }
+}
--- src/main/java/org/dl/tgbot/service/CreateStoriesMessageServiceImpl.java
+++ src/main/java/org/dl/tgbot/service/CreateStoriesMessageServiceImpl.java
@@ -1,9 +1,11 @@
 package org.dl.tgbot.service;
 
 import org.dl.tgbot.dto.*;
+import org.dl.tgbot.parse.Story;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +16,16 @@ public class CreateStoriesMessageServiceImpl implements CreateMessageService{
 
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
-        // todo: вставить Лизину логику сюда
+        // todo: вставить логику сюда возвращать тут названия историй через какой-то метод
 
-        String[] test = {"First", "Second", "Third", "Fourth", "Fifth", "Sixth"};
+        Story story = new Story();
+        ArrayList<String> test;
+        try {
+            test = story.printTitles();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         for (String s : test) {
             List<InlineKeyboardButton> rows = new ArrayList<>();
             rows.add(InlineKeyboardButton
